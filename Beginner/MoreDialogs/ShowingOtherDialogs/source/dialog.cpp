abbced72f4de5b0b2cc85f730d25d978814f16b1
Mustafa Selçuk Çağlar a.k.a. mcaglar a.k.a. develooper1994 <selcukcaglar08@gmail.com>
2023-03-31 13:18:12 +0300

    MoreDialogs is all about making gui application more than one dialogbox

---
+++ Beginner/MoreDialogs/ShowingOtherDialogs/source/dialog.cpp
@@ -0,0 +1,32 @@
+#include "../header/dialog.h"
+#include "./ui_dialog.h"
+
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+// slots
+void Dialog::on_btnWithParent_clicked(){
+    // dependent to parent window by passing this
+    Dialog2* dialog2 = new Dialog2(this); // creates and not destroys when out of scope!
+    //Dialog2 dialog2;  // creates and destroys when out of scope!
+    dialog2->show();
+}
+
+
+void Dialog::on_btnWithoutParent_clicked(){
+    // dependent to parent window by passing nullptr
+    Dialog2* dialog2 = new Dialog2(nullptr); // creates and not destroys when out of scope!
+    dialog2->show();
+}
+
