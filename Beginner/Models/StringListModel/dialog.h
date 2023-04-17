8d18786c2ba8651cece2db38523ad02f298dacbb
Mustafa Selçuk Çağlar a.k.a. mcaglar a.k.a. develooper1994 <mustafaselcuk.caglar@belbim.istanbul>
2023-04-17 11:19:37 +0300

    StringListModel finished

--- Beginner/Models/StringListModel/dialog.h
+++ Beginner/Models/StringListModel/dialog.h
@@ -2,6 +2,11 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QStringList>
+#include <QStringListModel>
+#include <QPushButton>
+#include <QInputDialog>
+#include <QModelIndex>
 
 
 
@@ -10,7 +15,6 @@ namespace Ui { class Dialog; }
 QT_END_NAMESPACE
 
 class Dialog : public QDialog
-
 {
     Q_OBJECT
 
@@ -18,8 +22,19 @@ public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_buttonBox_accepted();
+    void on_buttonBox_rejected();
+    void add();
+    void remove();
+
 private:
     Ui::Dialog *ui;
+
+    QStringList list;
+    QStringListModel model;
+
+    void init();
 };
 
 #endif // DIALOG_H
