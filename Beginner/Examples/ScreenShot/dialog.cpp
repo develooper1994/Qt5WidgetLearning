0dfa5a38860035c15a2bfc7ea3aa94737213ed00
Mustafa Selçuk Çağlar a.k.a. mcaglar a.k.a. develooper1994 <selcukcaglar08@gmail.com>
2023-04-29 00:18:26 +0300

    ScreenShot applicaiton started. works like clippy but it is sor screenshots

---
+++ Beginner/Examples/ScreenShot/dialog.cpp
@@ -0,0 +1,30 @@
+#include "dialog.h"
+#include "./ui_dialog.h"
+
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog(){
+    delete ui;
+}
+
+
+// slots
+void Dialog::on_btnScreen_clicked(){
+
+}
+
+
+void Dialog::on_btnSave_clicked(){
+
+}
+
+void Dialog::shoot(){
+
+}
+
